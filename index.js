d31488fa9cfd37f8e9e9239601bce406d591f7f7
Thomas Tursics <thomas@tursics.de>
2021-06-28 16:07:47 +0200

    change color of the lines dynamically

---
+++ index.js
@@ -0,0 +1,12 @@
+ddj.autostart.onDone(function() {
+//	ddj.map.get().scrollWheelZoom.disable();
+});
+
+ddj.autostart.onAddMarker(function(properties, value) {
+    if (properties.type === 'regio') {
+        properties.borderColor = 'red';
+        properties.borderWeight = 4;
+    }
+
+    return true;
+});

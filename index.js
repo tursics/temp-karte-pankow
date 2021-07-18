97216ac8dda8a60f86add087dd10b9b0b6dd1248
Thomas Tursics <thomas@tursics.de>
2021-07-19 00:34:52 +0200

    show selected area on map

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
 var userInput = {
-	areaId: '1100000303',
+	areaId: 'all',
 	areaTitle: 'Bezirk Pankow',
 };
+
+var layers = {
+    areas: []
+}
+
+function initLayers() {
+    var map = ddj.map.get();
+    map.eachLayer(function(layer) {
+        if (layer && layer.feature && layer.feature.properties && layer.feature.properties.bezeich && (layer.feature.properties.bezeich === 'AX_KommunalesGebiet')) {
+            layers.areas.push(layer);
+        }
+    });
+}
 
 function dataUpdated() {
 	'use strict';
 
-	if (ddj.getData() === null) {
+	if (ddj.data.get() === null) {
 		return;
 	}
 
-    ddj.marker.update();
+    var map = ddj.map.get();
+    var layerPrefix = '1100000303';
+    for (var l = 0; l < layers.areas.length; ++l) {
+        var layer = layers.areas[l];
+        if (userInput.areaId === 'all') {
+            map.removeLayer(layer);
+        } else {
+            map.removeLayer(layer);
+            if (layer.feature.properties.sch === (layerPrefix + userInput.areaId)) {
+                layer.options.fill = false;
+                layer.options.stroke = true;
+            } else {
+                layer.options.fill = true;
+                layer.options.stroke = false;
+            }
+            map.addLayer(layer);
+        }
+    }
+
+//    ddj.marker.update();
 }
 
 ddj.autostart.onDone(function() {
+    initLayers();
+
     $('#select-area')
         .change(function() {
             userInput.areaTitle = $('#select-area option:selected').text();
             userInput.areaId = $('#select-area option:selected').val();
+
+            dataUpdated();
         })
         .trigger('change');
 });
@@ -28,6 +64,5 @@ ddj.autostart.onAddMarker(function(properties, value) {
         properties.borderWeight = 4;
     }*/
 
-    console.log('x');
     return true;
 });
